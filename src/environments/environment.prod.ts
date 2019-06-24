c72186ef8ff3f2696f45383916f1a062797a5303
Thomas Moyse <tmoyse@gmail.com>
2019-06-24 21:39:46 +0200

    Add cors proxy for GitHub pages demo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
   expenseApi: {
-    url: "https://mobile.ilucca-dev.net/api/expenseItems",
+    //url: "https://mobile.ilucca-dev.net/api/expenseItems",
+    url:
+      "https://cors-anywhere.herokuapp.com/https://mobile.ilucca-dev.net/api/expenseItems",
     token: "793ed839b844d7b18667156be72d7eb0471ec706"
   },
   currencyApi: {
